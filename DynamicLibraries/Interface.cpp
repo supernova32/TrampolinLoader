77c6d4c64572d02ad0fa69e692025f63b488bc53
Patricio Cano <suprnova32@gmail.com>
2012-12-13 13:16:11 +0100

    First Commit

---
+++ DynamicLibraries/Interface.cpp
@@ -0,0 +1,9 @@
+//
+//  Interface.cpp
+//  DynamicLibraries
+//
+//  Created by Patricio Cano on 12/13/12.
+//  Copyright (c) 2012 Patricio Cano. All rights reserved.
+//
+
+#include "Interface.h"
---
+++ DynamicLibraries/Interface.h
@@ -0,0 +1,14 @@
+//
+//  Interface.h
+//  DynamicLibraries
+//
+//  Created by Patricio Cano on 12/13/12.
+//  Copyright (c) 2012 Patricio Cano. All rights reserved.
+//
+
+#ifndef DynamicLibraries_Interface_h
+#define DynamicLibraries_Interface_h
+
+
+
+#endif
