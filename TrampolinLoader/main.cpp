fc3cc70ccf379673cf7de94231386db14335f2f2
Patricio Cano <suprnova32@gmail.com>
2012-12-13 13:03:48 +0100

    Initial Commit

---
+++ TrampolinLoader/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TrampolinLoader
+//
+//  Created by Patricio Cano on 12/13/12.
+//  Copyright (c) 2012 Patricio Cano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
